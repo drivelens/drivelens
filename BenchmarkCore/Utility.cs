27aad890a1ae335bd9a8806a97deb139d374e266
peng1999 <pgw1999@foxmail.com>
2015-07-20 15:50:30 +0800

    添加ForEach工具方法

---
+++ BenchmarkCore/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkCore
+{
+    class Utility
+    {
+        public static IEnumerable<T> ForEach<T, TResult>(this IEnumerable<T> list, Func<T, TResult> work)
+        {
+            foreach (var item in list)
+            {
+                work(item);
+            }
+            return list;
+        }
+    }
+}
