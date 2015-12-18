28490c9392496fa1723c0659bf0e2c0a75b04a98
彭冠文 <pgw1041436954@outlook.com>
2015-12-18 14:42:56 +0800

    NativeMethods

---
+++ BenchLibrary/NativeMethods.fs
@@ -0,0 +1,9 @@
+﻿module Drivelens.BenchLibrary.NativeMethods
+
+open Microsoft.Win32.SafeHandles
+open System
+open System.IO
+open System.Runtime.InteropServices
+
+[<DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)>]
+extern SafeFileHandle CreateFile(string lpFileName, FileAccess dwDesiredAccess, FileShare dwShareMode, IntPtr lpSecurityAttributes, FileMode dwCreationDisposition, int32 dwFlagsAndAttributes, IntPtr hTemplateFile)
